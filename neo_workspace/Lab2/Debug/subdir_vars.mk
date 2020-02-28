497d28afa60e0872e4f3e844b42d29a13ecebe41
Meyer <alecrm4@iastate.edu>
2020-02-28 15:27:21 -0600

    Added lab6

--- neo_workspace/Lab2/Debug/subdir_vars.mk
+++ neo_workspace/Lab2/Debug/subdir_vars.mk
@@ -5,9 +5,6 @@
 SHELL = cmd.exe
 
 # Add inputs and outputs from these tool invocations to the build variables 
-CMD_SRCS += \
-../tm4c123gh6pm.cmd 
-
 C_SRCS += \
 ../Timer.c \
 ../lcd.c \
---
+++ neo_workspace/Lab6/adc.h
@@ -0,0 +1,21 @@
+/*
+ * adc.h
+ *
+ *  Created on: Feb 28, 2020
+ *      Author: alecrm4
+ */
+
+
+#ifndef ADC_H_
+#define ADC_H_
+#include <inc/tm4c123gh6pm.h>
+#include "Timer.h"
+#include "lcd.h"
+
+void adc_init();
+
+
+
+
+
+#endif /* ADC_H_ */
